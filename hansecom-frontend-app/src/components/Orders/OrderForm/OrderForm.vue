1e35c06221fdfa2e096831223d2b6b1f41c36a01
Gonçalo Mendes <65180829+goncalofm90@users.noreply.github.com>
2024-10-12 17:53:31 +0100

    gm-setup-routing placeholder component content

---
+++ hansecom-frontend-app/src/components/Orders/OrderForm/OrderForm.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div className="flex justify-center">
+    <h1 className="text-8xl text-center">Order Form</h1>
+  </div>
+</template>
---
+++ hansecom-frontend-app/src/components/Users/UserForm/UserForm.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div className="flex justify-center">
+    <h1 className="text-8xl text-center">User Form</h1>
+  </div>
+</template>
