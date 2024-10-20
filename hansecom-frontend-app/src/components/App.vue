a51372d2c6c8d4bb5df81005445cb908d89ead7d
Gonçalo Mendes <65180829+goncalofm90@users.noreply.github.com>
2024-10-20 15:27:48 +0100

    gm-design-and-styles remove placeholder routes

--- hansecom-frontend-app/src/components/App.vue
+++ hansecom-frontend-app/src/components/App.vue
@@ -1,8 +1,4 @@
 <template>
-  <div class="flex justify-between p-5">
-    <RouterLink to="/orders">Orders</RouterLink>
-    <RouterLink to="/users">Users</RouterLink>
-  </div>
   <RouterView />
 </template>
 
